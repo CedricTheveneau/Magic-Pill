1ba192730afb92e6f83f7c5f7f1acb52b56de8ff
CedricTheveneau <theveneaucedric@gmail.com>
2024-11-12 18:54:49 +0100

    Updated version

--- src/MagicPill.js
+++ src/MagicPill.js
@@ -1,7 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DOMPurify from "dompurify";
+import './magicpill.css';
 
-const MagicPill = ({ message }) => {
-  return <div>{message}</div>;
+import ArrowLabel from './icons/arrow_label.js';
+import BoxLabel from './icons/box_label.js';
+import CheckmarkLabel from './icons/checkmark_label.js';
+import CrossLabel from './icons/cross_label.js';
+import MailLabel from './icons/mail_label.js';
+import PlusLabel from './icons/plus_label.js';
+import QuotationLabel from './icons/quotation_label.js';
+import TelLabel from './icons/tel_label.js';
+import UndoLabel from './icons/undo_label.js';
+
+import CheckmarkIcon from './icons/checkmark.js';
+import CouponIcon from './icons/coupon.js';
+import InfoIcon from './icons/info.js';
+import QuestionmarkIcon from './icons/questionmark.js';
+
+const MagicPill = ({ pillData }) => {
+  const { icon, message, cta, info } = pillData;
+  const { icon: ctaIcon, label: ctaLabel, link: ctaLink } = cta || {};
+  const { title, content, closeLabel } = info || {};
+
+  const [collapsed, setCollapsed] = useState(true);
+  const [temporaryCTAIcon, setTemporaryCTAIcon] = useState(ctaIcon)
+
+  const createMarkup = (html) => {
+    return { __html: DOMPurify.sanitize(html) };
+  };
+
+  let IconComponent;
+  let CTAIconComponent;
+
+  switch (icon) {
+    case 'checkmark':
+      IconComponent = CheckmarkIcon;
+      break;
+    case 'coupon':
+      IconComponent = CouponIcon;
+      break;
+    case 'info':
+      IconComponent = InfoIcon;
+      break;
+    case 'questionmark':
+      IconComponent = QuestionmarkIcon;
+      break;
+    default:
+      IconComponent = null;
+  }
+
+  switch (ctaIcon) {
+    case 'arrow':
+      CTAIconComponent = ArrowLabel;
+      break;
+    case 'box':
+      CTAIconComponent = BoxLabel;
+      break;
+    case 'checkmark':
+      CTAIconComponent = CheckmarkLabel;
+      break;
+    case 'cross':
+      CTAIconComponent = CrossLabel;
+      break;
+    case 'mail':
+      CTAIconComponent = MailLabel;
+      break;
+    case 'plus':
+      CTAIconComponent = PlusLabel;
+      break;
+    case 'quotation':
+      CTAIconComponent = QuotationLabel;
+      break;
+    case 'tel':
+      CTAIconComponent = TelLabel;
+      break;
+    case 'undo':
+      CTAIconComponent = UndoLabel;
+      break;
+    default:
+      CTAIconComponent = null;
+  }
+
+  return (
+    <div className='magicPill'>
+      {IconComponent && <IconComponent className="icon"/>}
+      <p className='message'>{message}</p>
+      {ctaLink && !info && <a className='CTA' href={ctaLink}>{CTAIconComponent && <CTAIconComponent className="label" />}{" "}{ctaLabel}</a>}
+      {ctaLink && info && 
+  <button 
+    className='CTA' 
+    onClick={() => {
+      setCollapsed(!collapsed);
+      setTemporaryCTAIcon('cross');
+    }}
+  >
+    {CTAIconComponent && <CTAIconComponent className="label" />}
+    {" "}
+    {!temporaryCTAIcon === ctaIcon ? ctaLabel : closeLabel}
+  </button>
+}
+      {info && <div className='info'><p className='title'>{title}</p><div className='content' dangerouslySetInnerHTML={createMarkup(content)}></div></div>}
+    </div>
+  );
 };
 
 export default MagicPill;
