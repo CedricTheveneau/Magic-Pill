6a7ee3648683d5e70f98a016edecab9f3bcebc43
CedricTheveneau <theveneaucedric@gmail.com>
2026-02-05 21:53:06 +0100

    Restore old demo from local out/ directory

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as MagicPill } from './MagicPill';
-export * from './types';
-export * from './utils/handlers';
-export * from './utils/icons';
-export * from './defaultPillData';
--- src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export type { MagicPillNavbar, MagicPillNavbarItem } from './navbar';
-export type { MagicPillNotification, MagicPillNotificationCTA, MagicPillNotificationInfo } from './notification';
-export type { MagicPillType, Mode, NotificationIcon, CTAIcon } from './magicPill';
